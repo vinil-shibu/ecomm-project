7749b1f72c00aba9fb4f6683190dc802713effd0
Vinil Shibu <106754750+vinil-shibu@users.noreply.github.com>
2023-09-16 12:40:54 +0530

    Authentication completed

---
+++ src/app/seller-auth/seller-auth.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { SellerService } from '../services/seller.service';
+import { Router } from '@angular/router';
+import { SignUp } from 'src/data-type';
+@Component({
+  selector: 'app-seller-auth',
+  templateUrl: './seller-auth.component.html',
+  styleUrls: ['./seller-auth.component.css']
+})
+export class SellerAuthComponent implements OnInit {
+  constructor(private seller:SellerService, private router:Router) {}
+  ngOnInit(): void {
+      
+  }
+
+  signUp(data:SignUp):void{
+    this.seller.userSignUp(data).subscribe((result)=>{
+      if (result){
+        this.router.navigate(['seller-home'])
+      }
+    });
+  }
+
+}
