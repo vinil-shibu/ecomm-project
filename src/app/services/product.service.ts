ad31a1222fd47947388e6831f65a36dc93ec20f7
Vinil Shibu <106754750+vinil-shibu@users.noreply.github.com>
2023-09-19 18:15:31 +0530

    Displaying Product list with API

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -12,4 +12,8 @@ export class ProductService {
   addProduct(data:product){
     return this.http.post('http://localhost:3000/products',data);
   }
+
+  productList(){
+    return this.http.get<product[]>('http://localhost:3000/products')
+  }
 }
