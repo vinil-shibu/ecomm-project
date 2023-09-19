52c0c4e6d10ae942a828d197adcbfab5136947f2
Vinil Shibu <106754750+vinil-shibu@users.noreply.github.com>
2023-09-19 06:04:17 +0530

    Add Product API integration

--- src/app/seller-add-product/seller-add-product.component.ts
+++ src/app/seller-add-product/seller-add-product.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../services/product.service';
+import { product } from 'src/data-type';
+
 
 @Component({
   selector: 'app-seller-add-product',
@@ -6,7 +9,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./seller-add-product.component.css']
 })
 export class SellerAddProductComponent {
-  submit(data:object){
-    console.warn(data);
+  addProductMessage: string | undefined;
+
+  constructor(private product: ProductService) { }
+
+  submit(data: product) {
+    this.product.addProduct(data).subscribe((result) => {
+      console.warn(result);
+      if (result) {
+        this.addProductMessage = "Product Successfully Added"
+      }
+      setTimeout(() => {
+        this.addProductMessage = undefined
+      }, 3000);
+    })
   }
 }
--- src/data-type.ts
+++ src/data-type.ts
@@ -7,3 +7,12 @@ export interface Login{
     email:string,
     password:string
 }
+
+export interface product{
+    name:string,
+    price:number,
+    category:string,
+    description: string,
+    color:string,
+    image:string
+}
