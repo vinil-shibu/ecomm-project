68773971e6a9f33e87a03854524599d819dd3383
Vinil Shibu <106754750+vinil-shibu@users.noreply.github.com>
2023-09-25 20:12:43 +0530

    prefilling the update option

--- src/app/seller-update-product/seller-update-product.component.ts
+++ src/app/seller-update-product/seller-update-product.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../services/product.service';
+import { product } from 'src/data-type';
 
 @Component({
   selector: 'app-seller-update-product',
@@ -6,8 +9,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./seller-update-product.component.css']
 })
 export class SellerUpdateProductComponent {
+  productData:undefined|product
+  constructor (private route : ActivatedRoute, private product:ProductService){}
 
-
+  ngOnInit(): void {
+    let productId = this.route.snapshot.paramMap.get(`id`)
+    console.warn(productId);
+    productId && this.product.getProduct(productId).subscribe((data)=>{
+      console.warn(data)
+      this.productData=data
+    })
+    
+  }
   submit(data:any){
 
   }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -7,16 +7,22 @@ import { product } from 'src/data-type';
 })
 export class ProductService {
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-  addProduct(data:product){
-    return this.http.post('http://localhost:3000/products',data);
+  addProduct(data: product) {
+    return this.http.post('http://localhost:3000/products', data);
   }
 
-  productList(){
+  productList() {
     return this.http.get<product[]>('http://localhost:3000/products');
   }
-  deleteProduct(id:number){
+
+  deleteProduct(id: number) {
     return this.http.delete(`http://localhost:3000/products/${id}`);
   }
+
+  getProduct(id: string) {
+    return this.http.get<product>(`http://localhost:3000/products/${id}`);
+  }
+
 }
