d202d5e3672d47809a937d8bc0c20412f530b59a
Vinil Shibu <106754750+vinil-shibu@users.noreply.github.com>
2023-09-26 15:39:46 +0530

    Adding Bootstrap Carousel

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit{
+
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+
   constructor(){}
   ngOnInit(): void {
 
