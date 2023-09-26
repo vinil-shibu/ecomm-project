3b88ff0686c9b737ef499b3c8a669bcc49d9d49e
Vinil Shibu <106754750+vinil-shibu@users.noreply.github.com>
2023-09-26 20:27:00 +0530

    Dynamic Carousel

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../services/product.service';
+import { product } from 'src/data-type';
 
 @Component({
   selector: 'app-home',
@@ -7,11 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit{
 
+  popularProduct:undefined|product[];
   images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
 
-  constructor(){}
+  constructor(private product:ProductService){}
   ngOnInit(): void {
-
+    this.product.popularProducts().subscribe((data)=>{
+      console.warn(data);
+      this.popularProduct=data;
+    });
   }
       
 }
