16c165ae5d71b57defcee5f7a49fb507a6f8cb26
Vinil Shibu <106754750+vinil-shibu@users.noreply.github.com>
2023-10-30 19:09:53 +0530

    part 7 Dynamic Cart Page

--- src/app/cart-page/cart-page.component.ts
+++ src/app/cart-page/cart-page.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../services/product.service';
+import { cart, priceSummary } from 'src/data-type';
 
 @Component({
   selector: 'app-cart-page',
   templateUrl: './cart-page.component.html',
   styleUrls: ['./cart-page.component.css']
 })
-export class CartPageComponent {
+export class CartPageComponent implements OnInit{
+cartData:cart[]|undefined;
+priceSummary:priceSummary={
+  price:0,
+    discount:0,
+    tax:0,
+    delivery:0,
+    total:0
+}
+  constructor(private product:ProductService) {}
 
+  ngOnInit(): void {
+      this.product.currentCart().subscribe((result)=>{
+        this.cartData=result;
+        let price=0;
+        result.forEach((item)=>{
+          if(item.quantity){
+            price=price+(+item.price*+item.quantity);
+          }
+        });
+        this.priceSummary.price=price;
+        this.priceSummary.discount=price/10;
+        this.priceSummary.tax=Math.round((price/100)*18);
+        this.priceSummary.delivery=100;
+        this.priceSummary.total=Math.round(price-(price/10)+((price/100)*18)+100)
+      });
+  }
 }
